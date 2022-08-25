3877cc1000e47100f4ac832917713384d5b4cbb2
yeonjoo.lee <teddyjune@naver.com>
2022-08-25 17:27:26 +0900

    feat: viewModel 변수 수정

--- lib/ui/subway_view_model.dart
+++ lib/ui/subway_view_model.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:subway_search_app/data/data_source/subway_api_impl.dart';
 import 'package:subway_search_app/data/model/subway.dart';
 import 'package:subway_search_app/data/repository/subway_repository.dart';
 
 class SubwayViewModel extends ChangeNotifier {
-  final _repository = SubwayRepository();
+  final _repository = SubwayRepository(SubwayApiImpl());
   List<Subway> arrivalInfo = [];
 
   void fetchInfo(String query) async {
